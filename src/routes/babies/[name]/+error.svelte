76eff56ae7ce09c5e04f4e4b3ff8f183b4713bc5
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-10-01 11:50:12 +0200

    🥅 handle 404: baby not found

---
+++ src/routes/babies/[name]/+error.svelte
@@ -0,0 +1,1 @@
+Diese Seite existiert nicht. Wähle ein Baby oben oder gehe zur <a href="/">Startseite</a>
--- src/routes/babies/[name]/+page.ts
+++ src/routes/babies/[name]/+page.ts
@@ -4,13 +4,11 @@ import { doc, getDoc } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params }) => {
-	return getDoc(doc(firestore, 'babies', params.name))
-		.then((babyDocument) => {
-			if (!babyDocument.exists()) return error(404,{message:'Not found'});
-			return params
-		})
-		.catch((e) => {
-			console.log({ e });
-			error(500);
-		});
+	return getDoc(doc(firestore, 'babies', params.name)).then((babyDocument) => {
+		if (!babyDocument.exists()) {
+			// throw redirect(300, '/');
+			throw error(404, { message: 'Not found' });
+		}
+		return { name: params.name };
+	});
 };
