fdd52524dcf3301fd837a828ef59007f16b7a899
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-10-06 23:04:12 +0200

    🐛 do not access firebase on the server

--- src/routes/babies/[name]/+page.ts
+++ src/routes/babies/[name]/+page.ts
@@ -3,6 +3,8 @@ import { error } from '@sveltejs/kit';
 import { doc, getDoc } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 
+// don't exec on server because can't access firestore on server...
+export const ssr = false;
 export const load: PageLoad = ({ params }) => {
 	return getDoc(doc(firestore, 'babies', params.name)).then((babyDocument) => {
 		if (!babyDocument.exists()) {
